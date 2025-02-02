21a27351663233bba7b519e2a3f7c91f38ccc5fa
anudeep.madrampalli <anudeep571@gmail.com>
2025-02-02 12:25:59 +1100

    Organise files & DSA Stack
    DSA methods
    push() - Add element
    pop() - Delete top element
    peek() - Find top element
    search() - Position of element in stack
    empty() - Check if stack is empty

--- codewars/src/DataAnalyzer.java
+++ codewars/src/DataAnalyzer.java
@@ -22,7 +22,6 @@
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 
 public class DataAnalyzer {
---
+++ dsa/src/DSACheck.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+
+    int[] a = {4, 5, 5, 6, -7, -7};
+
+    for (int j : a) {
+        System.out.println(j);
+    }
+
+
+}

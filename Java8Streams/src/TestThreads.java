f1d0734012ad9ac91943f7601aff7b1e3f93c4c9
anudeep.madrampalli <anudeep571@gmail.com>
2025-02-11 19:20:37 +1100

    // Java Streams Basics
    1. Predicate
    2. Function
    3. Consumer
    4. Supplier
    5. BiPredicate
    6. BiConsumer
    7. BiFunction
    8. Method Reference
    9. Constructor Reference

--- Java8Streams/src/TestThreads.java
+++ Java8Streams/src/TestThreads.java
@@ -1,6 +1,7 @@
 public class TestThreads {
     public static void main(String[] args) {
 
+        // Lambda Expression
         Thread t1 = new Thread(() -> System.out.println("T1"));
         Thread t2 = new Thread(() -> System.out.println("T2"));
         Thread t3 = new Thread(() -> System.out.println("T3"));
